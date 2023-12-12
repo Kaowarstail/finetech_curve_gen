3cd4c57dc7d16375d761d2e4b1c330984bf29cf0
Kaowarstail <victor.dane@edu.devinci.fr>
2023-12-12 10:47:03 +0100

    dysplay sample data

--- main.py
+++ main.py
@@ -1,61 +1,18 @@
-import yfinance as yf
-import pandas as pd
-import numpy as np
+import nasdaqdatalink
 import requests
-import json
+import json 
+import matplotlib.pyplot as plt
 
-msft = yf.Ticker("MSFT")
+res = requests.get('https://data.nasdaq.com/api/v3/datasets/FRED/GDP.json')
+response = json.loads(res.text)
+data = response['dataset']['data']
 
-# get all stock info
-msft.info
+dates = [row[0] for row in data]
+values = [row[1] for row in data]
 
-# get historical market data
-hist = msft.history(period="1mo")
-
-# show meta information about the history (requires history() to be called first)
-msft.history_metadata
-
-# show actions (dividends, splits, capital gains)
-msft.actions
-msft.dividends
-msft.splits
-msft.capital_gains  # only for mutual funds & etfs
-
-# show share count
-msft.get_shares_full(start="2022-01-01", end=None)
-
-# show financials:
-# - income statement
-msft.income_stmt
-msft.quarterly_income_stmt
-# - balance sheet
-msft.balance_sheet
-msft.quarterly_balance_sheet
-# - cash flow statement
-msft.cashflow
-msft.quarterly_cashflow
-# see `Ticker.get_income_stmt()` for more options
-
-# show holders
-msft.major_holders
-msft.institutional_holders
-msft.mutualfund_holders
-
-# Show future and historic earnings dates, returns at most next 4 quarters and last 8 quarters by default. 
-# Note: If more are needed use msft.get_earnings_dates(limit=XX) with increased limit argument.
-msft.earnings_dates
-
-# show ISIN code - *experimental*
-# ISIN = International Securities Identification Number
-msft.isin
-
-# show options expirations
-msft.options
-
-# show news
-msft.news
-
-# get option chain for specific expiration
-opt = msft.option_chain('YYYY-MM-DD')
-# data available via: opt.calls, opt.puts
-
+plt.plot(dates, values)
+plt.xlabel('Date')
+plt.ylabel('GDP (Billions of Dollars)')
+plt.title('Gross Domestic Product')
+plt.xticks(rotation=45)
+plt.show()
