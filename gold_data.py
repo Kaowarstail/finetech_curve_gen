e792cf9ecdca2a52dcc6cade00683b7738bd045c
Kaowarstail <victor.dane@edu.devinci.fr>
2023-12-13 16:24:06 +0100

    Generation of data to feed for the IA

---
+++ gold_data.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import pandas as pd
+import random
+import datetime
+
+import os
+from dotenv import load_dotenv
+
+output_folder = '/home/kaowarstail/Documents/finetech/finetech_curve_gen/screenshot/'
+data_file = '/home/kaowarstail/Documents/finetech/finetech_curve_gen/Data OR.xlsx'  # Chemin vers le fichier de données
+
+# Charger les données depuis le fichier Excel
+df = pd.read_excel(data_file)
+
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
+# Define the start and end dates
+start_date = datetime.date(2006, 1, 9)
+end_date = datetime.date(2023, 11, 3)
+
+# Maintenant, vous pouvez sélectionner les données en utilisant des dates
+# df_selected = df.loc[start_date:end_date]
+
+
+
+# Define the maximum duration in days (3 months)
+max_duration = datetime.timedelta(days=90)
+
+# Generate random start dates within the range
+num_periods = 50  # Number of periods to generate
+periods = []
+for _ in range(num_periods):
+    duration = random.randint(60, max_duration.days)
+    start = start_date + datetime.timedelta(days=random.randint(0, (end_date - start_date).days - duration))
+    end = start + datetime.timedelta(days=duration)
+    periods.append((start, end))
+
+# Define market colors
+mc = mpf.make_marketcolors(up='g', down='r', wick={'up':'g', 'down':'r'}, volume='inherit')
+
+# Define style
+s  = mpf.make_mpf_style(marketcolors=mc)
+
+
+# Execute the code for each period
+for start, end in periods:
+    start_date_str = start.strftime("%Y-%m-%d")
+    end_date_str = end.strftime("%Y-%m-%d")
+    # Update the code to use the selected start and end dates
+    start_datetime = datetime.datetime.strptime(start_date_str, "%Y-%m-%d")
+    end_datetime = datetime.datetime.strptime(end_date_str, "%Y-%m-%d")
+    df_selected = df.loc[end_datetime:start_datetime]
+    
+    # Check if df_selected is not empty
+    if not df_selected.empty:
+        filename = f'screenshot/graph_{start_date_str}_{end_date_str}.png'
+        mpf.plot(df_selected, type='candle', style=s, title='Gold Price', ylabel='Gold Price (USD)', savefig=filename)
+    else:
+        print(f"No data available for the period {start_date_str} to {end_date_str}")
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import matplotlib.pyplot as plt
+import mplfinance as mpf
+import pandas as pd
 import requests
 import json 
 import random
 import datetime
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Prend l'environnement à partir de .env
+
+NASDAQ_DATA_LINK_API_KEY = os.getenv('NASDAQ_DATA_LINK_API_KEY')
+
 start_date = datetime.date(1970, 1, 1)
 end_date = datetime.date.today()
 
-num_queries = 5  # Nombre de requêtes à effectuer
+num_queries = 2  # Nombre de requêtes à effectuer
 output_folder = '/home/kaowarstail/Documents/finetech/finetech_curve_gen/screenshot/'
 
 for _ in range(num_queries):
@@ -21,24 +30,28 @@ for _ in range(num_queries):
     query_end_str = query_end.strftime('%Y-%m-%d')
 
     # Effectuer la requête
-    res = requests.get(f'https://data.nasdaq.com/api/v3/datasets/LBMA/GOLD.json?start_date={query_start_str}&end_date={query_end_str}')
+    res = requests.get(f'https://data.nasdaq.com/api/v3/datasets/LBMA/GOLD.json?start_date={query_start_str}&end_date={query_end_str}&api_key={NASDAQ_DATA_LINK_API_KEY}')
     response = json.loads(res.text)
+    # print(response)
     data = response['dataset']['data']
 
     dates = [row[0] for row in data[::-1]]  # Inverser l'ordre des dates
     values = [row[1] for row in data[::-1]]  # Inverser l'ordre des valeurs
 
-    # Tracer le graphique
-    plt.plot(dates, values)
-    plt.xlabel('Date')
-    plt.ylabel('Gold Price (USD)')
-    plt.title('Gold Price: London Fixing')
-    plt.xticks(rotation=45)
+    # Créer un DataFrame avec les données
+    df = pd.DataFrame(data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Column6', 'Column7'])
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
 
     # Générer un nom de fichier unique pour chaque capture
     filename = f'graph_{query_start_str}_{query_end_str}.png'
     filepath = output_folder + filename
 
+    df = df.dropna(subset=['Open', 'High', 'Low', 'Close'])
+
+
+    # Tracer le graphique chandelier
+    mpf.plot(df, type='candle', title='Gold Price: London Fixing', ylabel='Gold Price (USD)', savefig=filepath)
     # Sauvegarder le graphique en haute qualité
     plt.savefig(filepath, dpi=300)
 
